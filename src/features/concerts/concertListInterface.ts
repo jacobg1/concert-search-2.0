fa428b07f78f6f5c103cf3b69c6351959a72e604
Jacob Greenwald <greenwald.j8@gmail.com>
2021-11-27 08:00:06 -0500

    Updating interface

--- src/features/concerts/concertListInterface.ts
+++ src/features/concerts/concertListInterface.ts
@@ -1,10 +1,24 @@
 import { ChangeEvent } from 'react'
 
+export type AccordionHandler = (
+  id: string
+) => (event: React.SyntheticEvent, isExpanded: boolean) => void
+
+export interface ConcertAccordionProps {
+  identifier: string
+  title: string
+  description: string
+  expanded: string | false
+  handleChange: AccordionHandler
+}
+
 export type PaginationHandler = (
   event: ChangeEvent<unknown>,
   value: number
 ) => void
 
-export type AccordionHandler = (
-  id: string
-) => (event: React.SyntheticEvent, isExpanded: boolean) => void
+export interface ConcertPaginationProps {
+  count: number
+  pageNumber: number
+  handlePageChange: PaginationHandler
+}
