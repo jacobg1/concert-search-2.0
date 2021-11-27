e28cbe95365dbcf27c42a6f013edc00de701a53e
Jacob Greenwald <greenwald.j8@gmail.com>
2021-11-27 11:51:39 -0500

    Adding todo comment

--- src/features/concertSelect/concertSelectSlice.ts
+++ src/features/concertSelect/concertSelectSlice.ts
@@ -46,6 +46,7 @@ const { loadBandList } = concertSelectSlice.actions
 export const { selectBand, selectYear, clearBand, clearYear } =
   concertSelectSlice.actions
 
+// TODO: load this from back end
 export const handleLoadBandList = (): AppThunk => (dispatch) => {
   dispatch(loadBandList(artistYearList))
 }
