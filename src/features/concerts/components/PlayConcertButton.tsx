062d83ae68ee24befae3c90fee8032282ec9768e
Jacob Greenwald <greenwald.j8@gmail.com>
2021-11-27 18:07:05 -0500

    Adding button to play concert

---
+++ src/features/concerts/components/PlayConcertButton.tsx
@@ -0,0 +1,27 @@
+import { Button } from '@mui/material'
+import PlayArrowIcon from '@mui/icons-material/PlayArrow'
+
+interface PlayConcertButtonProps {
+  playConcert: (identifier: string) => void
+  identifier: string
+}
+
+export default function PlayConcertButton({
+  playConcert,
+  identifier,
+}: PlayConcertButtonProps) {
+  return (
+    <Button
+      sx={{
+        display: 'flex',
+        alignItems: 'start',
+      }}
+      variant="contained"
+      color="primary"
+      onClick={() => playConcert(identifier)}
+      endIcon={<PlayArrowIcon fontSize="small" />}
+    >
+      Play
+    </Button>
+  )
+}
