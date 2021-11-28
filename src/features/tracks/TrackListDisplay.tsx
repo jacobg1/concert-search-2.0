a2ae406d7b97cd128415f9248522caf036b29c50
Jacob Greenwald <greenwald.j8@gmail.com>
2021-11-28 01:24:22 -0500

    Render tracklist and track meta info

--- src/features/tracks/TrackListDisplay.tsx
+++ src/features/tracks/TrackListDisplay.tsx
@@ -1,11 +1,37 @@
+import { List } from '@mui/material'
+import { SxProps } from '@mui/system'
+import SingleTrack from './components/SingleTrack'
 import { TrackListData } from './trackInterface'
+
+const listContainerStyles: SxProps = {
+  width: '90%',
+  margin: '5px auto',
+  maxHeight: '60vh',
+  overflowY: 'auto',
+  '& .MuiButtonBase-root': {
+    '&:first-of-type': {
+      marginTop: 0,
+    },
+    '&:last-of-type': {
+      marginBottom: 0,
+    },
+  },
+}
 
 interface TrackListDisplayProps {
   trackList: TrackListData[]
 }
+
+// TODO: order by track# in back-end
 export default function TrackListDisplay({
   trackList,
 }: TrackListDisplayProps): JSX.Element | null {
   if (!trackList.length) return null
-  return <h3>render trackList</h3>
+  return (
+    <List disablePadding sx={listContainerStyles}>
+      {trackList.map((track) => {
+        return <SingleTrack key={track.name} {...track} />
+      })}
+    </List>
+  )
 }
