3bbf8e44b3b529b62ba1c29e62b7ea54f5e353e7
Jacob Greenwald <greenwald.j8@gmail.com>
2021-11-29 18:26:14 -0500

    Adding play/pause and volume functionality

--- src/features/player/AudioPlayer.tsx
+++ src/features/player/AudioPlayer.tsx
@@ -1,42 +1,71 @@
-import { useRef, useState } from 'react'
+import { Box, Stack } from '@mui/material'
+import { useEffect, useRef, useState } from 'react'
+// import { TrackListData } from '../tracks/trackInterface'
 import { AudioElement } from './components/AudioElement'
+import PlayOrPause from './components/PlayOrPause'
+import VolumeSlider from './components/VolumeSlider'
+import { background } from '../../app/background'
+import { SxProps } from '@mui/system'
+import { useAppDispatch, useAppSelector } from '../../app/hooks'
+import { setPlayerState } from '../selectedConcert/selectedConcertSlice'
 
-export default function AudioPlayer() {
+const audioPlayerStyles: SxProps = {
+  padding: 2,
+  width: '90%',
+  border: '2px solid black',
+  background,
+  boxSizing: 'border-box',
+}
+
+export default function AudioPlayer({ src }: { src: string }) {
+  const dispatch = useAppDispatch()
   const audioEl = useRef<HTMLAudioElement>(null)
-  const [playerState, setPlayerState] = useState('play')
-  const onClick = () => {
-    const { current } = audioEl
-    if (!current) return null
+  // const [isPlaying, setIsPlaying] = useState(false)
+  const [volume, setVolume] = useState<number>(25)
+  const {
+    playerState,
+    currentlyPlayingTrack: { playUrl },
+  } = useAppSelector((state) => state.individualConcert)
 
-    if (current.paused) {
-      setPlayerState('pause')
-      current.play()
-    } else {
-      setPlayerState('play')
-      current.pause()
-    }
-  }
-  const playOrPause = () => {
+  useEffect(() => {
     const { current } = audioEl
-    if (!current) return 'Play'
-    return current.paused ? 'Pause' : 'Play'
+    if (!current) return
+
+    // Set initial volume
+    current.volume = (volume as number) / 100
+
+    // If playerState changes, play or pause accordingly
+    if (playerState === 'play') current.play()
+    else current.pause()
+  }, [playerState, playUrl])
+
+  const onPlayPauseClick = () => {
+    const { current } = audioEl
+    if (!current) return
+
+    // Toggle playerState
+    if (current.paused) dispatch(setPlayerState('play'))
+    else dispatch(setPlayerState('pause'))
   }
+
+  const handleVolumeChange = (event: Event, newValue: number | number[]) => {
+    const { current } = audioEl
+    if (!current) return
+
+    current.volume = (newValue as number) / 100
+    setVolume(newValue as number)
+  }
+
   return (
-    <>
-      <p onClick={onClick}>{playerState}</p>
-      <AudioElement
-        // controls={false}
-        ref={audioEl}
-        src="https://ia600501.us.archive.org/13/items/moe2011-03-25.salvo/moe2011-03-25d2t-03.mp3"
-      />
-      {/* <audio
-        controls={false}
-        ref={(el) => (audioEl.current = el)}
-        id="musicPlayer"
-        preload="metadata"
-        crossOrigin="anonymous"
-        src="https://ia600501.us.archive.org/13/items/moe2011-03-25.salvo/moe2011-03-25d2t-03.mp3"
-      /> */}
-    </>
+    <Box my={3} sx={audioPlayerStyles}>
+      <AudioElement ref={audioEl} src={src} />
+      <Stack flexDirection="row" justifyContent="space-between">
+        <PlayOrPause
+          isPlaying={playerState === 'play'}
+          onPlayPauseClick={onPlayPauseClick}
+        />
+        <VolumeSlider volume={volume} handleVolumeChange={handleVolumeChange} />
+      </Stack>
+    </Box>
   )
 }
