860d941bfc57589bbf135543e0234bd2f6a652a9
Jacob Greenwald <greenwald.j8@gmail.com>
2021-11-29 18:25:35 -0500

    Adding some new components for the custom audio player

---
+++ src/features/player/components/VolumeSlider.tsx
@@ -0,0 +1,59 @@
+import { VolumeUp } from '@mui/icons-material'
+import { Stack, Slider, Box, Button, Popover, Popper } from '@mui/material'
+import ClickAwayListener from '@mui/material/ClickAwayListener'
+
+import { usePopover } from '../../../app/hooks'
+
+type ClickAwayEvent = Event | React.SyntheticEvent
+
+interface VolumeSliderProps {
+  // audioPlayer: HTMLAudioElement | null
+  volume: number
+  handleVolumeChange: (event: Event, newValue: number | number[]) => void
+}
+
+export default function VolumeSlider({
+  volume,
+  handleVolumeChange,
+}: VolumeSliderProps) {
+  const [htmlEl, isOpen, handleOpen, handleClose] =
+    usePopover<HTMLButtonElement>()
+
+  return (
+    <Box>
+      <Button
+        color="primary"
+        variant="contained"
+        size="small"
+        aria-label="volume-control"
+        sx={{
+          minWidth: '40px',
+          padding: '4px 6px',
+        }}
+        onClick={handleOpen}
+      >
+        <VolumeUp />
+      </Button>
+      <Popper
+        placement="top-end"
+        id={'vol-control'}
+        open={isOpen}
+        anchorEl={htmlEl}
+      >
+        {() => (
+          <ClickAwayListener
+            onClickAway={handleClose as (event: ClickAwayEvent) => void}
+          >
+            <Stack bgcolor="#bed5ff" width="200px">
+              <Slider
+                sx={{ width: '90%', margin: 'auto', padding: '20px 0' }}
+                value={volume}
+                onChange={handleVolumeChange}
+              />
+            </Stack>
+          </ClickAwayListener>
+        )}
+      </Popper>
+    </Box>
+  )
+}
