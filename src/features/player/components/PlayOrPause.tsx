5313f3dd99b150bb2d1b0dd556a72b7d79642b61
Jacob Greenwald <greenwald.j8@gmail.com>
2021-11-30 20:02:48 -0500

    Logic to show play or pause button

--- src/features/player/components/PlayOrPause.tsx
+++ src/features/player/components/PlayOrPause.tsx
@@ -1,3 +1,30 @@
-export default function PlayOrPause() {
-  return <div />
+import PauseSharpIcon from '@mui/icons-material/PauseSharp'
+import PlayArrowSharpIcon from '@mui/icons-material/PlayArrowSharp'
+import { Button, Box } from '@mui/material'
+
+interface PlayOrPauseProps {
+  isPlaying: boolean
+  onPlayPauseClick: () => void
+}
+
+export default function PlayOrPause({
+  isPlaying,
+  onPlayPauseClick,
+}: PlayOrPauseProps) {
+  return (
+    <Box onClick={onPlayPauseClick}>
+      <Button
+        color="primary"
+        variant="contained"
+        size="small"
+        aria-label={isPlaying ? 'pause' : 'play'}
+        sx={{
+          minWidth: '40px',
+          padding: '4px 6px',
+        }}
+      >
+        {isPlaying ? <PauseSharpIcon /> : <PlayArrowSharpIcon />}
+      </Button>
+    </Box>
+  )
 }
