9af0f9b7468af8eaf93fab5ab60f87d549b4da16
Jacob Greenwald <greenwald.j8@gmail.com>
2021-11-30 19:59:42 -0500

    usePopover hook

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -1,5 +1,28 @@
+import { useState, MouseEvent } from 'react'
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 import type { RootState, AppDispatch } from './store'
 
 export const useAppDispatch = () => useDispatch<AppDispatch>()
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+
+type PopoverHandler<T> = (event: MouseEvent<T>) => void
+
+export function usePopover<T>(): [
+  T | null,
+  boolean,
+  PopoverHandler<T>,
+  PopoverHandler<T>
+] {
+  const [htmlEl, setHtmlEl] = useState<T | null>(null)
+  const isOpen = Boolean(htmlEl)
+
+  const handleOpen = (event: MouseEvent<T>) => {
+    setHtmlEl(event.currentTarget)
+  }
+
+  const handleClose = (event: MouseEvent<T>) => {
+    setHtmlEl(null)
+  }
+
+  return [htmlEl, isOpen, handleOpen, handleClose]
+}
