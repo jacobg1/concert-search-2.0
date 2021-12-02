83ca0b1e6bade1de8fb249c88b287d76c49be479
Jacob Greenwald <greenwald.j8@gmail.com>
2021-12-02 18:05:29 -0500

    Moving audio player logic to container component to pass down props to both TrackListDisplay and AudioPlayer

--- src/features/selectedConcert/SelectedConcertDisplay.tsx
+++ src/features/selectedConcert/SelectedConcertDisplay.tsx
@@ -1,9 +1,17 @@
+import { useEffect, useRef, useState } from 'react'
 import { CircularProgress, Drawer, Stack } from '@mui/material'
 import { SxProps } from '@mui/system'
-import { useAppSelector } from '../../app/hooks'
+import { useAppDispatch, useAppSelector } from '../../app/hooks'
 import TrackListDisplay from '../tracks/TrackListDisplay'
 import ConcertMeta from '../tracks/components/ConcertMeta'
 import AudioPlayer from '../player/AudioPlayer'
+import { AudioElement } from '../player/components/AudioElement'
+import {
+  playNextTrack,
+  playPreviousTrack,
+  setPlayerState,
+  playNewTrack,
+} from './selectedConcertSlice'
 
 const drawerStyles: SxProps = {
   height: '100%',
@@ -16,7 +24,7 @@ const drawerStyles: SxProps = {
   },
   '& .MuiDrawer-paperAnchorBottom': {
     backgroundColor: '#2e7e89',
-    overflow: 'hidden',
+    overflow: 'auto',
   },
 }
 
@@ -29,9 +37,92 @@ export default function SelectedConcertDisplay({
 }: SelectedConcertDisplayProps): JSX.Element {
   const {
     loading,
-    currentlyPlayingTrack,
+    playerState,
+    currentlyPlayingTrack: { playUrl, currentTrackName },
     selectedConcert: { trackList, metaData },
   } = useAppSelector((state) => state.individualConcert)
+
+  const dispatch = useAppDispatch()
+  const audioEl = useRef<HTMLAudioElement>(null)
+  const [volume, setVolume] = useState<number>(25)
+
+  useEffect(() => {
+    const { current } = audioEl
+
+    if (current) {
+      current.volume = (volume as number) / 100
+    }
+  }, [volume, playerState])
+
+  useEffect(() => {
+    const { current } = audioEl
+    if (!current) return
+
+    if (playerState === 'play') {
+      current.play()
+    } else {
+      current.pause()
+    }
+  }, [playerState, playUrl])
+
+  const isPlaying = (current: HTMLAudioElement): boolean => {
+    return !!(
+      current.currentTime > 0 &&
+      !current.paused &&
+      !current.ended &&
+      current.readyState > 2
+    )
+  }
+
+  const handlePlayNewTrack = (name: string): void => {
+    const { current } = audioEl
+    if (!current) return
+
+    if (!playUrl) {
+      dispatch(playNewTrack(name))
+      return
+    }
+
+    if (current.readyState > 2) {
+      dispatch(playNewTrack(name))
+      return
+    }
+  }
+
+  const onPlayPauseClick = (): void => {
+    const { current } = audioEl
+    if (!current) return
+
+    if (!isPlaying(current)) {
+      dispatch(setPlayerState('play'))
+    } else {
+      dispatch(setPlayerState('pause'))
+    }
+  }
+
+  const handleNextTrack = (): void => {
+    const { current } = audioEl
+
+    if (current && current.readyState > 2) {
+      dispatch(playNextTrack())
+    }
+  }
+
+  const handlePreviousTrack = (): void => {
+    const { current } = audioEl
+
+    if (current && current.readyState > 2) {
+      dispatch(playPreviousTrack())
+    }
+  }
+
+  const handleVolumeChange = (event: Event, newValue: number | number[]) => {
+    const { current } = audioEl
+
+    if (current) {
+      setVolume(newValue as number)
+    }
+  }
 
   return (
     <Drawer
@@ -56,12 +147,28 @@ export default function SelectedConcertDisplay({
                 source={metaData.source}
               />
             )}
-            {trackList.length && (
+            {trackList.length ? (
               <>
-                <TrackListDisplay trackList={trackList} />
-                <AudioPlayer src={currentlyPlayingTrack.playUrl} />
+                <AudioElement
+                  handleNextTrack={handleNextTrack}
+                  ref={audioEl}
+                  src={playUrl}
+                />
+                <TrackListDisplay
+                  trackList={trackList}
+                  playNewTrack={handlePlayNewTrack}
+                  currentTrackName={currentTrackName}
+                />
+                <AudioPlayer
+                  volume={volume}
+                  playerState={playerState}
+                  onPlayPauseClick={onPlayPauseClick}
+                  handleNextTrack={handleNextTrack}
+                  handlePreviousTrack={handlePreviousTrack}
+                  handleVolumeChange={handleVolumeChange}
+                />
               </>
-            )}
+            ) : null}
           </>
         )}
       </Stack>
