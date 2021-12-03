9e49728c005deeda1bd2901c952271d3c3f4b4de
Jacob Greenwald <greenwald.j8@gmail.com>
2021-12-02 23:57:13 -0500

    Starting to work on progress bar;

--- src/features/player/components/ProgressBar.tsx
+++ src/features/player/components/ProgressBar.tsx
@@ -1,1 +1,29 @@
-export default {}
+import { useState } from 'react'
+import { Box, Slider } from '@mui/material'
+
+const progressBarStyles = {
+  width: '100%',
+  '& #progressBar': {
+    width: '100%',
+    padding: '20px 0',
+  },
+}
+
+export default function ProgressBar({ duration }: { duration: number }) {
+  const [position, setPosition] = useState(0)
+  return (
+    <Box sx={progressBarStyles}>
+      <Slider
+        aria-label="progress-bar"
+        id="progressBar"
+        // size="small"
+        value={position}
+        min={0}
+        step={1}
+        max={duration}
+        onChange={(e, value) => setPosition(value as number)}
+        // sx={}
+      />
+    </Box>
+  )
+}
