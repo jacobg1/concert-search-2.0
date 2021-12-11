412260830c4d2c6e786cc2f36146e52d65debf4c
Jacob Greenwald <greenwald.j8@gmail.com>
2021-12-11 17:09:56 -0500

    Volume button component and removing ClickAwayListener

--- src/features/player/components/VolumeSlider.tsx
+++ src/features/player/components/VolumeSlider.tsx
@@ -1,57 +1,67 @@
 import { Stack, Slider, Box, Button, Popper } from '@mui/material'
 import { VolumeUp } from '@mui/icons-material'
-import ClickAwayListener from '@mui/material/ClickAwayListener'
-
+import CloseSharpIcon from '@mui/icons-material/CloseSharp'
 import { usePopover } from '../../../app/hooks'
-
-type ClickAwayEvent = Event | React.SyntheticEvent
+import { PopoverHandler } from '../../../app/interface'
 
 interface VolumeSliderProps {
   volume: number
   handleVolumeChange: (event: Event, newValue: number | number[]) => void
 }
 
+interface VolumeButtonProps {
+  isOpen: boolean
+  clickHandler: PopoverHandler<HTMLButtonElement>
+}
+
+const VolumeButton = ({
+  isOpen,
+  clickHandler,
+}: VolumeButtonProps): JSX.Element => {
+  return (
+    <Button
+      color="primary"
+      variant="contained"
+      size="small"
+      aria-label="volume-control"
+      sx={{
+        minWidth: '40px',
+        padding: '4px 6px',
+      }}
+      onClick={clickHandler}
+    >
+      {isOpen ? <CloseSharpIcon /> : <VolumeUp />}
+    </Button>
+  )
+}
 export default function VolumeSlider({
   volume,
   handleVolumeChange,
-}: VolumeSliderProps) {
+}: VolumeSliderProps): JSX.Element {
   const [htmlEl, isOpen, handleOpen, handleClose] =
     usePopover<HTMLButtonElement>()
 
   return (
     <Box>
-      <Button
-        color="primary"
-        variant="contained"
-        size="small"
-        aria-label="volume-control"
-        sx={{
-          minWidth: '40px',
-          padding: '4px 6px',
-        }}
-        onClick={!isOpen ? handleOpen : handleClose}
-      >
-        <VolumeUp />
-      </Button>
+      <VolumeButton
+        isOpen={isOpen}
+        clickHandler={!isOpen ? handleOpen : handleClose}
+      />
       <Popper
         id="vol-control"
         placement="top-end"
         open={isOpen}
         anchorEl={htmlEl}
       >
-        {() => (
-          <ClickAwayListener
-            onClickAway={handleClose as (event: ClickAwayEvent) => void}
-          >
-            <Stack bgcolor="#bed5ff" width="200px">
-              <Slider
-                sx={{ width: '90%', margin: 'auto', padding: '20px 0' }}
-                value={volume}
-                onChange={handleVolumeChange}
-              />
-            </Stack>
-          </ClickAwayListener>
-        )}
+        <Stack bgcolor="#bed5ff">
+          <Slider
+            sx={{ width: '225px', margin: '11px' }}
+            value={volume}
+            min={0}
+            max={100}
+            onChange={handleVolumeChange}
+          />
+        </Stack>
       </Popper>
     </Box>
   )
