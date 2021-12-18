40518aad1e043345c8e59a10d4bf5ec434c90ad0
Jacob Greenwald <greenwald.j8@gmail.com>
2021-12-18 14:19:39 -0500

    Adding some types

--- src/app/interface.ts
+++ src/app/interface.ts
@@ -3,3 +3,10 @@ import { Dispatch, SetStateAction, MouseEvent } from 'react'
 export type SetState<T> = Dispatch<SetStateAction<T>>
 
 export type PopoverHandler<T> = (event: MouseEvent<T>) => void
+
+export type VolumeChangeHandler = (
+  event: Event,
+  newValue: number | number[]
+) => void
+
+export type SongPositionHandler = (songPosition: number) => void
