baa6ce26535cb966f73f080a48215c18084d9ada
Jacob Greenwald <greenwald.j8@gmail.com>
2021-12-19 09:02:51 -0500

    Remove unused types

--- src/features/concertSelect/concertSelectInterface.ts
+++ src/features/concertSelect/concertSelectInterface.ts
@@ -1,22 +1,14 @@
 import { SelectChangeEvent } from '@mui/material'
-import { SyntheticEvent } from 'react'
 
 export type SelectEvent = SelectChangeEvent<{ label: string } | null>
-
-export type InputClearHandler = (
-  event: SyntheticEvent,
-  newInputValue: string,
-  reason: string
-) => void
 
 export interface ConcertSelectProps {
   id: string
   placeholder: string
-  autocompleteOptions: { label: string }[]
-  changeHandler: (selection: string) => void
-  clearHandler: () => void
   value: string
   disabled: boolean
+  autocompleteOptions: { label: string }[]
+  changeHandler: (selection: string) => void
 }
 
 export interface ConcertSelectButtonProps {
