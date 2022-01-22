1555877141c36c6fa6d3e85f79e91753e432a832
Jacob Greenwald <greenwald.j8@gmail.com>
2022-01-22 17:45:06 -0500

    Prevent concert search api call if search is same as previous

--- src/features/concerts/concertListSlice.ts
+++ src/features/concerts/concertListSlice.ts
@@ -12,14 +12,26 @@ type ChunkedConcertList = SingleConcertMeta[][]
 
 interface ConcertListState {
   concerts: ChunkedConcertList
+  concertBand: string
+  concertYear: string
   loading: boolean
   error: Error | Record<string, unknown>
   pageNumber: number
 }
 
-export const fetchConcertList = createAsyncThunk(
+export const fetchConcertList = createAsyncThunk<
+  ChunkedConcertList,
+  Record<string, string>,
+  {
+    state: { concertList: ConcertListState }
+  }
+>(
   'concertList/fetchConcertList',
-  async (query: Record<string, string>) => {
+  async (query, thunkApi) => {
+    const { dispatch } = thunkApi
+
+    dispatch(setBandAndYear(query))
+
     const { bandName, year } = query
     const response = await fetch(
       `${process.env.REACT_APP_BASE_URL}/meta/${bandName}/${year}`,
@@ -28,11 +40,26 @@ export const fetchConcertList = createAsyncThunk(
       }
     )
     return response.json() as Promise<ChunkedConcertList>
+  },
+  {
+    condition: ({ bandName, year }, { getState }) => {
+      const {
+        concertList: { concertBand, concertYear },
+      } = getState()
+
+      if (bandName === concertBand && year === concertYear) {
+        return false
+      }
+
+      return true
+    },
   }
 )
 
 const initialState: ConcertListState = {
   concerts: [],
+  concertBand: '',
+  concertYear: '',
   loading: false,
   error: {},
   pageNumber: 1,
@@ -44,6 +71,10 @@ const concertListSlice = createSlice({
   reducers: {
     setPageNumber: (state, action: PayloadAction<number>) => {
       state.pageNumber = action.payload
+    },
+    setBandAndYear: (state, action: PayloadAction<{ [k: string]: string }>) => {
+      state.concertBand = action.payload.bandName
+      state.concertYear = action.payload.year
     },
   },
   extraReducers: {
@@ -70,5 +101,5 @@ const concertListSlice = createSlice({
   },
 })
 
-export const { setPageNumber } = concertListSlice.actions
+export const { setPageNumber, setBandAndYear } = concertListSlice.actions
 export default concertListSlice.reducer
