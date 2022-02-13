3d4a3ff9e773bc2c76a3dde1340dd666ff4e15d2
Jacob Greenwald <greenwald.j8@gmail.com>
2022-02-13 13:50:03 -0500

    Render snackbar

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,14 @@ import SelectedConcertDisplay from './features/selectedConcert/SelectedConcertDi
 import { AppHeader } from './AppHeader'
 import { theme } from './app/theme'
 import { AppStyles } from './AppStyles'
+import { ErrorDisplay } from './ErrorDisplay'
 
 function App(): JSX.Element {
   return (
     <ThemeProvider theme={theme}>
       <AppStyles theme={theme} />
       <Box>
+        <ErrorDisplay />
         <AppHeader />
         <BandAndYearSelect />
         <ConcertListDisplay />
