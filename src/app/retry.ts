6718f59fa0de43b2ddfc7885911920dc860ad965
Jacob Greenwald <greenwald.j8@gmail.com>
2022-02-13 19:04:46 -0500

    Retry middleware

---
+++ src/app/retry.ts
@@ -0,0 +1,17 @@
+import { Middleware, MiddlewareAPI } from '@reduxjs/toolkit'
+import { fetchConcertList } from '../features/concerts/concertListSlice'
+import { AppDispatch, RootState } from './store'
+
+// Retry initial search twice if lambda is sleeping
+let tries = 0
+const extraTries = 2
+export const retry: Middleware =
+  (store: MiddlewareAPI<AppDispatch, RootState>) => (next) => (action) => {
+    if (action.type === 'concertList/fetchConcertList/rejected') {
+      tries += 1
+      if (tries <= extraTries) {
+        return store.dispatch(fetchConcertList(action.meta.arg))
+      }
+    }
+    return next(action)
+  }
