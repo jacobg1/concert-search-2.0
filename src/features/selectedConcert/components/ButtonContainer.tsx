9793b67cc6421337e65738abcb32424fae220af3
Jacob Greenwald <greenwald.j8@gmail.com>
2022-02-19 00:09:37 -0500

    Adding song format selector

--- src/features/selectedConcert/components/ButtonContainer.tsx
+++ src/features/selectedConcert/components/ButtonContainer.tsx
@@ -1,5 +1,6 @@
 import { Box } from '@mui/material'
 import { IconDirection } from '../../../app/interface'
+import SongFormatSelect from '../../tracks/components/SongFormatSelect'
 import { BackButton } from './BackButton'
 
 export function ButtonContainer(): JSX.Element {
@@ -8,10 +9,13 @@ export function ButtonContainer(): JSX.Element {
       style={{
         width: '90%',
         margin: '30px auto 20px',
-        textAlign: 'left',
+        display: 'flex',
+        justifyContent: 'space-between',
+        // alignItems: 'center',
       }}
     >
       <BackButton iconDirection={IconDirection.Left} />
+      <SongFormatSelect />
     </Box>
   )
 }
