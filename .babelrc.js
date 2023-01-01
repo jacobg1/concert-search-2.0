a40e3198f0a20efdfaadbd3700a3968285d84bdc
jacobg1 <greenwald.j8@gmail.com>
2023-01-01 13:13:56 -0500

    Update babel config

--- .babelrc.js
+++ .babelrc.js
@@ -1,8 +1,8 @@
 const plugins = [
   [
     'babel-plugin-direct-import',
-    { modules: ['@mui/material', '@mui/icons-material'] },
+    { modules: ['@mui/system', '@mui/material', '@mui/icons-material'] },
   ],
-];
+]
 
-module.exports = { plugins };
+module.exports = { plugins }
