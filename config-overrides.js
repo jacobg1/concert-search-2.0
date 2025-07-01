667aa2b6d67a7ae65cad922c9e53df5a1815dfbb
jacobg1 <greenwald.j8@gmail.com>
2025-07-01 13:45:50 -0400

    update dependencies

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,4 @@
 /* config-overrides.js */
-/* eslint-disable react-hooks/rules-of-hooks */
-const { useBabelRc, override } = require('customize-cra');
+import { useBabelRc, override } from 'customize-cra'
 
-module.exports = override(useBabelRc());
+export default override(useBabelRc())
