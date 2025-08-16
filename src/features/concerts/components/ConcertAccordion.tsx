b2f6a79bd174ca9ea620f163d63ad361da33234d
jacobg1 <greenwald.j8@gmail.com>
2025-08-16 17:32:47 -0400

    fix text clip issue on mobile

--- src/features/concerts/components/ConcertAccordion.tsx
+++ src/features/concerts/components/ConcertAccordion.tsx
@@ -4,6 +4,7 @@ import {
   AccordionDetails,
   Typography,
   Box,
+  type SxProps,
 } from '@mui/material'
 
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
@@ -11,6 +12,18 @@ import { ConcertAccordionProps } from '../concertListInterface'
 import PlayConcertButton from './PlayConcertButton'
 import { fetchSelectedConcert } from '../../selectedConcert/selectedConcertSlice'
 import { useAppDispatch } from '../../../app/hooks'
+
+const summaryStyles: SxProps = {
+  '& .MuiAccordionSummary-content': {
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
+  },
+  '& .MuiTypography-body1': {
+    fontSize: '1.2rem',
+    fontWeight: 600,
+  },
+}
 
 export default function ConcertAccordion({
   identifier: concertId,
@@ -36,17 +49,7 @@ export default function ConcertAccordion({
         aria-controls={`concert-${concertId}-header`}
         expandIcon={<ExpandMoreIcon color="primary" />}
         slotProps={{ root: { component: Box } }}
-        sx={{
-          '& .MuiAccordionSummary-content': {
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            width: '100%',
-          },
-          '& .MuiTypography-body1': {
-            fontSize: '1.2rem',
-            fontWeight: 600,
-          },
-        }}
+        sx={summaryStyles}
       >
         <Typography component="span" width="67%">
           {title}
@@ -58,9 +61,12 @@ export default function ConcertAccordion({
         {source && (
           <>
             <Typography variant="subtitle1" style={{ marginBottom: '3px' }}>
-              Source
+              Source!
             </Typography>
-            <Typography variant="body1" style={{ marginBottom: '20px' }}>
+            <Typography
+              variant="body1"
+              style={{ marginBottom: '20px', overflow: 'hidden' }}
+            >
               {source}
             </Typography>
           </>
