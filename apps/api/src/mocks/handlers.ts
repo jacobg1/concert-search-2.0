a8813f791ccfeec9e8f4ddb8af6864035b3f1235
jacobg1 <greenwald.j8@gmail.com>
2025-10-14 17:05:24 -0400

    fix linter error

--- apps/api/src/mocks/handlers.ts
+++ apps/api/src/mocks/handlers.ts
@@ -2,11 +2,16 @@ import { http, HttpResponse } from 'msw'
 import { singleConcert } from './data/singleConcertResponse'
 import { concertList } from './data/concertListResponse'
 
+function getUrl(url?: string): string {
+  if (!url) throw new Error('missing mock url')
+  return url
+}
+
 export const handlers = [
-  http.get(process.env.METADATA_URL, () => {
+  http.get(getUrl(process.env.METADATA_URL), () => {
     return HttpResponse.json(singleConcert)
   }),
-  http.get(process.env.ADVANCED_SEARCH_URL, () => {
+  http.get(getUrl(process.env.ADVANCED_SEARCH_URL), () => {
     return HttpResponse.json(concertList)
   }),
 ]
