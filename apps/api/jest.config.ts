f0c6a42541385c419a70853939941a10d487b766
jacobg1 <greenwald.j8@gmail.com>
2025-10-17 12:00:42 -0400

    update api jest config

--- apps/api/jest.config.ts
+++ apps/api/jest.config.ts
@@ -2,19 +2,9 @@
 import { nestJestConfig } from '@repo/jest-config/nest'
 import type { Config } from 'jest'
 
-const coverage = nestJestConfig.coverageThreshold?.global ?? {}
-
 const config: Config = {
   ...nestJestConfig,
-  setupFiles: ['<rootDir>/test/mocks/archiveSearch.ts'],
-  coverageThreshold: {
-    global: {
-      ...coverage,
-      functions: 0,
-      branches: 10,
-      lines: 29,
-    },
-  },
+  setupFiles: ['./test/mocks/archiveSearch.ts'],
 }
 
 export default config
