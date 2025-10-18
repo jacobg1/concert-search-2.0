3f66adce712c908a399eb5c4120bfe44a3c8813e
jacobg1 <greenwald.j8@gmail.com>
2025-10-18 08:43:08 -0400

    add some unit test coverage

--- apps/api/test/unit/concertService.test.ts
+++ apps/api/test/unit/concertService.test.ts
@@ -1,11 +1,12 @@
 import { archiveSearch } from 'archive-search'
-import { singleConcert } from '@repo/mock-data/api'
+import { concertList, singleConcert } from '@repo/mock-data/post-api'
 import { INestApplicationContext } from '@nestjs/common'
 import { AppModule } from '../../src/app.module'
 import { ConcertService } from '../../src/services'
 import { bootstrap } from '../utils'
+import { MediaFormat, SortOrder } from '../../src/interface'
 
-// const mockSearch = jest.spyOn(archiveSearch, 'search')
+const mockSearch = jest.spyOn(archiveSearch, 'search')
 const mockMetaSearch = jest.spyOn(archiveSearch, 'metaSearch')
 
 const mockConcertId = '4'
@@ -20,14 +21,51 @@ describe('ConcertService Unit Tests', () => {
     concertService = app.get(ConcertService)
   })
 
-  it('getSingleConcert properly retrieves a single concert', async () => {
+  afterEach(() => {
+    jest.restoreAllMocks()
+  })
+
+  it('getConcertList properly retrieves a concert list', async () => {
+    mockSearch.mockResolvedValue(concertList)
+
+    const mockInput = {
+      searchTerm: 'TestSearch',
+      max: 50,
+      filterDuplicates: false,
+      sortBy: { downloads: SortOrder.DESC },
+      mediaFormat: ['mp3' as MediaFormat, 'ogg' as MediaFormat],
+    }
+
+    const response = await concertService.getConcertList({ body: mockInput })
+
+    expect(response.length).toBeGreaterThan(0)
+
+    response.forEach((page) => {
+      expect(page.length).toBeGreaterThan(0)
+    })
+  })
+
+  xit('getSingleConcert properly retrieves a single concert', async () => {
     mockMetaSearch.mockResolvedValue(singleConcert)
 
-    const response = await concertService.getSingleConcert({
+    const { trackList, metadata } = await concertService.getSingleConcert({
       pathParameters: { id: mockConcertId },
     })
 
-    expect(response.trackList.length).toBeGreaterThan(0)
+    expect(trackList.length).toBeGreaterThan(0)
     expect(mockMetaSearch).toHaveBeenCalledWith(mockConcertId)
+
+    trackList.forEach(({ title, link, length }) => {
+      expect(title).toBeDefined()
+      expect(link).toBeDefined()
+      expect(length).toBeDefined()
+      expect(link).toContain('https://')
+    })
+
+    const metadataFields = ['creator', 'date', 'description', 'venue', 'source']
+
+    for (const field of metadataFields) {
+      expect(metadata[field as keyof typeof metadata]).toBeDefined()
+    }
   })
 })
