30555fd0a4ade76195cf91d0fbc1fb7d6d792335
jacobg1 <greenwald.j8@gmail.com>
2025-10-20 12:43:04 -0400

    add test helpers

--- apps/api/test/utils/helpers.ts
+++ apps/api/test/utils/helpers.ts
@@ -1,4 +1,78 @@
+import {
+  MediaFormat,
+  SortOrder,
+  type SingleConcert,
+  type ConcertData,
+  type PaginatedConcertList,
+  type ConcertSearchOptions,
+} from '../../src/interface'
+
 export function isDefinedAs(typeName: string, val?: unknown): void {
   expect(val).toBeDefined()
   expect(typeof val).toBe(typeName)
+}
+
+export function getApiBaseUrl(): string {
+  const url = process.env.API_BASE_URL
+  if (!url) {
+    throw new Error('Missing api base url')
+  }
+  return url
+}
+
+export function getMockInput(searchTerm: string): ConcertSearchOptions {
+  return {
+    searchTerm,
+    max: 50,
+    filterDuplicates: false,
+    sortBy: { downloads: SortOrder.DESC },
+    mediaFormat: ['mp3' as MediaFormat, 'ogg' as MediaFormat],
+  }
+}
+
+export function testSingleConcert({ trackList, metadata }: ConcertData): void {
+  expect(trackList.length).toBeGreaterThan(0)
+
+  trackList.forEach(({ title, link, length }) => {
+    expect(title).toBeDefined()
+    expect(link).toBeDefined()
+    expect(length).toBeDefined()
+    // TODO - more comprehensive test for link
+    expect(link).toContain('https://')
+  })
+
+  const metadataFields = ['creator', 'date', 'description', 'venue', 'source']
+
+  for (const field of metadataFields) {
+    expect(metadata[field as keyof typeof metadata]).toBeDefined()
+  }
+}
+
+const concertListFields = [
+  'identifier',
+  'date',
+  'description',
+  'mediatype',
+  'source',
+  'title',
+]
+
+export function testConcertList(list: PaginatedConcertList): void {
+  expect(list.length).toBeGreaterThan(0)
+
+  list.forEach((page) => {
+    expect(page.length).toBeGreaterThan(0)
+
+    page.forEach((concert) => {
+      concertListFields.forEach((field) => {
+        isDefinedAs('string', concert[field as keyof SingleConcert])
+      })
+
+      const { mediatype, format } = concert
+
+      expect(mediatype).toBe('etree')
+      expect(format).toContain(MediaFormat.MP3)
+      expect(format).toContain(MediaFormat.OGG)
+    })
+  })
 }
