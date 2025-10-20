1bd0fb9bf65f2d0de2613e5da81d0d43ee4191ee
jacobg1 <greenwald.j8@gmail.com>
2025-10-20 12:49:44 -0400

    add basic unit and integration tests

--- apps/api/test/integration/concertSearch.spec.ts
+++ apps/api/test/integration/concertSearch.spec.ts
@@ -1,31 +1,63 @@
 import { INestApplicationContext } from '@nestjs/common'
+import nock from 'nock'
+import { concertList, singleConcert } from '@repo/mock-data/pre-api'
 import { AppModule } from '../../src/app.module'
 import { ConcertService } from '../../src/services'
-import { bootstrap } from '../utils'
+import {
+  bootstrap,
+  getApiBaseUrl,
+  getMockInput,
+  testConcertList,
+  testSingleConcert,
+} from '../utils'
 
-// TODO - finish and unskip
-describe.skip('Concert Search Integration', () => {
+const mockConcertId = '2'
+const mockSearchTerm = 'testing'
+
+describe('Concert Search Integration', () => {
   let app: INestApplicationContext
+  let concertService: ConcertService
 
-  beforeEach(async () => {
-    app = await bootstrap(AppModule)
-  })
-
-  afterEach(() => {
+  beforeAll(() => {
     jest.restoreAllMocks()
   })
 
+  beforeEach(async () => {
+    app = await bootstrap(AppModule)
+    concertService = app.get(ConcertService)
+  })
+
+  afterEach(() => {
+    nock.cleanAll()
+  })
+
   it('concertService properly initializes', () => {
-    const concertService = app.get(ConcertService)
     expect(concertService).toBeDefined()
     expect(concertService).toBeInstanceOf(ConcertService)
   })
 
-  it('concertService properly initializes', async () => {
-    const concertService = app.get(ConcertService)
-    const test = await concertService.getSingleConcert({
-      pathParameters: { id: '2' },
+  it('getConcertList returns the correct response', async () => {
+    nock(getApiBaseUrl())
+      .get('/advancedsearch.php')
+      .query(({ q }) => !!q?.includes(mockSearchTerm))
+      .reply(200, concertList)
+
+    const resp = await concertService.getConcertList({
+      body: getMockInput(mockSearchTerm),
     })
-    console.log('TEST', test)
+
+    testConcertList(resp)
+  })
+
+  it('getSingleConcert returns the correct response', async () => {
+    nock(getApiBaseUrl())
+      .get(`/metadata/${mockConcertId}`)
+      .reply(200, singleConcert)
+
+    const resp = await concertService.getSingleConcert({
+      pathParameters: { id: mockConcertId },
+    })
+
+    testSingleConcert(resp)
   })
 })
--- apps/api/test/unit/concertService.test.ts
+++ apps/api/test/unit/concertService.test.ts
@@ -2,70 +2,66 @@ import { archiveSearch } from 'archive-search'
 import { concertList, singleConcert } from '@repo/mock-data/post-api'
 import { INestApplicationContext } from '@nestjs/common'
 import { AppModule } from '../../src/app.module'
-import { ConcertService } from '../../src/services'
-import { bootstrap } from '../utils'
-import { MediaFormat, SortOrder } from '../../src/interface'
+import { baseOptions, ConcertService } from '../../src/services'
+import {
+  bootstrap,
+  getMockInput,
+  testConcertList,
+  testSingleConcert,
+} from '../utils'
 
 const mockSearch = jest.spyOn(archiveSearch, 'search')
 const mockMetaSearch = jest.spyOn(archiveSearch, 'metaSearch')
 
 const mockConcertId = '4'
+const mockSearchTerm = 'TestSearch'
 
-// TODO - finish these tests
 describe('ConcertService Unit Tests', () => {
   let app: INestApplicationContext
   let concertService: ConcertService
+
+  beforeAll(() => {
+    jest.mock('archive-search')
+  })
 
   beforeEach(async () => {
     app = await bootstrap(AppModule)
     concertService = app.get(ConcertService)
   })
 
-  afterEach(() => {
+  afterAll(() => {
     jest.restoreAllMocks()
   })
 
   it('getConcertList properly retrieves a concert list', async () => {
     mockSearch.mockResolvedValue(concertList)
 
-    const mockInput = {
-      searchTerm: 'TestSearch',
-      max: 50,
-      filterDuplicates: false,
-      sortBy: { downloads: SortOrder.DESC },
-      mediaFormat: ['mp3' as MediaFormat, 'ogg' as MediaFormat],
-    }
+    const mockInput = getMockInput(mockSearchTerm)
 
-    const response = await concertService.getConcertList({ body: mockInput })
-
-    expect(response.length).toBeGreaterThan(0)
-
-    response.forEach((page) => {
-      expect(page.length).toBeGreaterThan(0)
+    const resp = await concertService.getConcertList({
+      body: getMockInput(mockSearchTerm),
     })
+
+    expect(mockSearch).toHaveBeenCalledWith(mockSearchTerm, {
+      ...baseOptions,
+      max: mockInput.max,
+      sortBy: mockInput.sortBy,
+    })
+
+    expect(resp.length).toBeGreaterThan(0)
+
+    testConcertList(resp)
   })
 
-  xit('getSingleConcert properly retrieves a single concert', async () => {
+  it('getSingleConcert properly retrieves a single concert', async () => {
     mockMetaSearch.mockResolvedValue(singleConcert)
 
-    const { trackList, metadata } = await concertService.getSingleConcert({
+    const resp = await concertService.getSingleConcert({
       pathParameters: { id: mockConcertId },
     })
 
-    expect(trackList.length).toBeGreaterThan(0)
     expect(mockMetaSearch).toHaveBeenCalledWith(mockConcertId)
 
-    trackList.forEach(({ title, link, length }) => {
-      expect(title).toBeDefined()
-      expect(link).toBeDefined()
-      expect(length).toBeDefined()
-      expect(link).toContain('https://')
-    })
-
-    const metadataFields = ['creator', 'date', 'description', 'venue', 'source']
-
-    for (const field of metadataFields) {
-      expect(metadata[field as keyof typeof metadata]).toBeDefined()
-    }
+    testSingleConcert(resp)
   })
 })
