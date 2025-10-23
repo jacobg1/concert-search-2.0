477c305664aeda950687bf5899c71f22925057ca
jacobg1 <greenwald.j8@gmail.com>
2025-10-23 14:56:31 -0400

    update coverage percentages

--- apps/api/jest.config.ts
+++ apps/api/jest.config.ts
@@ -9,10 +9,10 @@ const config: Config = {
   collectCoverageFrom: ['**/src/**/*.{ts,tsx}', ...coverageCollect],
   coverageThreshold: {
     global: {
-      branches: 40,
-      functions: 55,
-      lines: 60,
-      statements: 60,
+      branches: 75,
+      functions: 80,
+      lines: 80,
+      statements: 80,
     },
   },
   setupFiles: ['dotenv/config'],
