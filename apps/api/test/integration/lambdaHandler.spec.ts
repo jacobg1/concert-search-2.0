96541782f0655bc34549b718df63f37d750e6cdc
jacobg1 <greenwald.j8@gmail.com>
2025-10-23 17:37:02 -0400

    add lambda error handling tests

--- apps/api/test/integration/lambdaHandler.spec.ts
+++ apps/api/test/integration/lambdaHandler.spec.ts
@@ -1,5 +1,5 @@
 import nock from 'nock'
-import type { APIGatewayProxyStructuredResultV2 } from 'aws-lambda'
+import type { APIGatewayProxyEventV2 } from 'aws-lambda'
 import { concertList, singleConcert } from '@repo/mock-data/pre-api'
 import { createMockEvent, createMockContext } from '@repo/mock-data/event'
 import {
@@ -9,10 +9,12 @@ import {
   testLambdaResponse,
   testConcertList,
   expectedHeaders,
+  expectedErrorHeaders,
 } from '../utils'
 import { handler } from '../../src/main'
 import type { ConcertData, PaginatedConcertList } from '../../src/interface'
-import { HttpStatus } from '@nestjs/common'
+import { HttpStatus, Logger } from '@nestjs/common'
+import type { TestLambdaResponse } from '../types'
 
 const mockConcertId = '001'
 const mockSearchTerm = 'handler test'
@@ -21,13 +23,16 @@ const concertsRoute = '/concerts'
 const mockContext = createMockContext()
 const mockCallback = () => null
 
+const errorLogMock = jest.spyOn(Logger, 'error')
+
 describe('Lambda Handler Integration', () => {
-  beforeAll(() => {
-    jest.restoreAllMocks()
+  afterEach(() => {
+    jest.resetAllMocks()
+    nock.cleanAll()
   })
 
-  afterEach(() => {
-    nock.cleanAll()
+  afterAll(() => {
+    jest.restoreAllMocks()
   })
 
   it('lambda handler can get a list of concerts', async () => {
@@ -47,7 +52,7 @@ describe('Lambda Handler Integration', () => {
       mockEvent,
       mockContext,
       mockCallback
-    )) as APIGatewayProxyStructuredResultV2
+    )) as TestLambdaResponse
 
     testLambdaResponse(response, {
       expectedStatusCode: HttpStatus.OK,
@@ -79,7 +84,7 @@ describe('Lambda Handler Integration', () => {
       mockEvent,
       mockContext,
       mockCallback
-    )) as APIGatewayProxyStructuredResultV2
+    )) as TestLambdaResponse
 
     testLambdaResponse(response, {
       expectedStatusCode: HttpStatus.OK,
@@ -93,5 +98,48 @@ describe('Lambda Handler Integration', () => {
     const concertData = JSON.parse(response.body) as ConcertData
 
     testSingleConcert(concertData)
+  })
+
+  it('lambda handler throws error if input is invalid', async () => {
+    errorLogMock.mockReturnThis()
+
+    const response = (await handler(
+      {} as APIGatewayProxyEventV2,
+      mockContext,
+      mockCallback
+    )) as TestLambdaResponse
+
+    testLambdaResponse(response, {
+      expectedStatusCode: HttpStatus.NOT_FOUND,
+      expectedHeaders: expectedErrorHeaders,
+      expectedBody: { message: 'Invalid request' },
+    })
+
+    expect(errorLogMock).toHaveBeenCalledTimes(1)
+  })
+
+  it('lambda handler throws an error if route is not found', async () => {
+    errorLogMock.mockReturnThis()
+
+    const mockEvent = createMockEvent({
+      method: 'POST',
+      route: '/invalid/route',
+      path: concertsRoute,
+      body: getMockInput(mockSearchTerm),
+    })
+
+    const response = (await handler(
+      mockEvent,
+      mockContext,
+      mockCallback
+    )) as TestLambdaResponse
+
+    testLambdaResponse(response, {
+      expectedStatusCode: HttpStatus.NOT_FOUND,
+      expectedHeaders: expectedErrorHeaders,
+      expectedBody: { message: 'Not found' },
+    })
+
+    expect(errorLogMock).toHaveBeenCalledTimes(1)
   })
 })
