10a19c8062a1679f3830631ac3b6168bd8e22e86
jacobg1 <greenwald.j8@gmail.com>
2025-10-27 17:13:40 -0400

    initial test setup

---
+++ apps/web/jest.config.ts
@@ -0,0 +1,28 @@
+/** @jest-config-loader esbuild-register */
+import { nextJestConfig } from '@repo/jest-config/next'
+import type { Config } from 'jest'
+import nextJest from 'next/jest.js'
+
+const createNextConfig = nextJest({ dir: './' })
+
+const coverageCollect = nextJestConfig.collectCoverageFrom ?? []
+
+const config: Config = {
+  ...nextJestConfig,
+  collectCoverageFrom: [
+    '**/src/**/*.{ts,tsx}',
+    '**/*Interface.{ts,tsx}',
+    ...coverageCollect,
+  ],
+  setupFilesAfterEnv: ['./test/config/setup.ts'],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 25,
+      lines: 55,
+      statements: 55,
+    },
+  },
+}
+
+export default createNextConfig(config)
