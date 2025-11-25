4619f2ae4a40da3a726a053da9be3f8981336eca
jacobg1 <greenwald.j8@gmail.com>
2025-11-25 15:46:58 -0500

    ignore file

--- apps/web/jest.config.ts
+++ apps/web/jest.config.ts
@@ -14,6 +14,7 @@ const config: Config = {
     '!**/background.ts',
     '!**/store.ts',
     '!**/theme.ts',
+    '!**/useRedux.ts',
     ...coverageCollect,
   ],
   setupFilesAfterEnv: ['./test/config/setup.ts'],
