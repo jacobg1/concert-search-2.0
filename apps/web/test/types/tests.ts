59c4800cb737b2f78f81f8eb8124c0c0d6f7459b
jacobg1 <greenwald.j8@gmail.com>
2025-11-26 12:30:42 -0500

    update types

--- apps/web/test/types/tests.ts
+++ apps/web/test/types/tests.ts
@@ -1,7 +1,17 @@
+import { PlayerState } from '../../src/app/interface'
 import type { RootState, AppStore } from '../../src/app/store'
 import type { RenderOptions } from '@testing-library/react'
 
 export interface ContextRenderOptions extends RenderOptions {
   preloadedState?: Partial<RootState>
   store?: AppStore
+}
+
+export interface UsePlayPauseArgs {
+  url: string
+  state: PlayerState
+}
+
+export interface UseSongDurationArgs {
+  url?: string
 }
