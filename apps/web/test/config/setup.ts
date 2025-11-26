a34568424917e6bebf56dff71dc40832d86aae7f
jacobg1 <greenwald.j8@gmail.com>
2025-11-26 12:30:31 -0500

    update test initialization logic

--- apps/web/test/config/setup.ts
+++ apps/web/test/config/setup.ts
@@ -1,16 +1,21 @@
 import '@testing-library/jest-dom'
+import { SessionState } from '../../src/app/interface'
 
-Object.assign(navigator, {
-  mediaSession: {
-    playbackState: 'paused',
-    setActionHandler: jest.fn(),
-    metadata: {},
-  },
-})
+beforeEach(() => {
+  jest.clearAllMocks()
 
-Object.assign(window, {
-  innerHeight: 300,
-  innerWidth: 300,
-  addEventListener: jest.fn(),
-  removeEventListener: jest.fn(),
+  Object.assign(navigator, {
+    mediaSession: {
+      playbackState: SessionState.Paused,
+      setActionHandler: jest.fn(),
+      metadata: {},
+    },
+  })
+
+  Object.assign(window, {
+    innerHeight: 300,
+    innerWidth: 300,
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+  })
 })
