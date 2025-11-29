283e1d123eeb2ea923703f014c9b292e2917edf4
jacobg1 <greenwald.j8@gmail.com>
2025-11-29 09:42:37 -0500

    update jest coverage %'s

--- apps/web/jest.config.ts
+++ apps/web/jest.config.ts
@@ -20,10 +20,10 @@ const config: Config = {
   setupFilesAfterEnv: ['./test/config/setup.ts'],
   coverageThreshold: {
     global: {
-      branches: 70,
-      functions: 25,
-      lines: 55,
-      statements: 55,
+      branches: 90,
+      functions: 50,
+      lines: 70,
+      statements: 70,
     },
   },
 }
