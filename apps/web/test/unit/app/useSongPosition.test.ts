d81756384a5a901c085925c18f0295bdeb2d73a9
jacobg1 <greenwald.j8@gmail.com>
2025-12-11 12:10:32 -0500

    remove unused import

--- apps/web/test/unit/app/useSongPosition.test.ts
+++ apps/web/test/unit/app/useSongPosition.test.ts
@@ -1,4 +1,4 @@
-import { act, waitFor } from '@testing-library/react'
+import { act } from '@testing-library/react'
 import { useSongPosition } from '../../../src/app/hooks'
 import {
   checkMediaSession,
