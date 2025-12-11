e9088b2571aef3696072a77c58464b093bcc4ea4
jacobg1 <greenwald.j8@gmail.com>
2025-12-11 13:30:25 -0500

    add test utils method

--- apps/web/test/utils/concerts.ts
+++ apps/web/test/utils/concerts.ts
@@ -1,12 +1,16 @@
+import { waitFor } from '@testing-library/react'
 import type { UserEvent } from '@testing-library/user-event'
-import type { GetAllByRole, GetByText } from '../types'
+import type {
+  ConcertListItemMatchers,
+  ConcertListItemText,
+  SearchConcertMatchers,
+} from '../types'
 
 const search = 'Search'
 
 export async function searchConcerts(
   user: UserEvent,
-  getByText: GetByText,
-  getAllByRole: GetAllByRole
+  { getByText, getAllByRole }: SearchConcertMatchers
 ) {
   const [firstInput] = getAllByRole('combobox')
 
@@ -15,4 +19,20 @@ export async function searchConcerts(
   const [, firstBandOption] = getAllByRole('option')
   await user.click(firstBandOption)
   await user.click(getByText(search))
+}
+
+export async function testConcertListItem(
+  user: UserEvent,
+  { title, source, description }: ConcertListItemText,
+  { findByText, queryByText }: ConcertListItemMatchers
+) {
+  await user.click(await findByText(title))
+
+  expect(await findByText(source)).toBeVisible()
+  expect(await findByText(description)).toBeVisible()
+
+  await user.click(await findByText(title))
+
+  await waitFor(() => expect(queryByText(source)).toBeNull())
+  await waitFor(() => expect(queryByText(description)).toBeNull())
 }
