e20c9610e9187d9bdaf722e77dff5b1508ff2b9b
jacobg1 <greenwald.j8@gmail.com>
2026-03-11 16:56:58 -0400

    increase integration test coverage

--- apps/web/test/integration/selectedConcert.test.tsx
+++ apps/web/test/integration/selectedConcert.test.tsx
@@ -4,15 +4,15 @@ import { mockFetch, searchConcerts, userRender } from '../utils'
 import App from '../../src/App'
 
 let fetchMock: jest.SpyInstance
-let mockPlay: jest.SpyInstance<Promise<void>>
 let mockPause: jest.SpyInstance<void>
+
+const errorText = "Oops! Something went wrong, please update search and try again."
 
 describe('Selected Concert Integration', () => {
   beforeEach(() => {
     jest.restoreAllMocks()
 
     fetchMock = jest.spyOn(global, 'fetch')
-    mockPlay = jest.spyOn(HTMLMediaElement.prototype, 'play')
     mockPause = jest.spyOn(HTMLMediaElement.prototype, 'pause')
   })
 
@@ -34,5 +34,38 @@ describe('Selected Concert Integration', () => {
     singleConcert.trackList.forEach(({ title }) => {
       expect(screen.getByText(title))
     })
+  })
+
+   it('Can find and view a concert', async () => {
+    mockPause.mockReturnValue()
+    mockFetch(fetchMock, concertList, true)
+    mockFetch(fetchMock, singleConcert, true)
+
+    const { user } = userRender(<App />)
+
+    await searchConcerts(user)
+    const [firstConcert] = screen.getAllByText('Play')
+    await user.click(firstConcert)
+
+    singleConcert.trackList.forEach(({ title }) => {
+      expect(screen.getByText(title))
+    })
+  })
+
+  it("Error text shows if single concert search fails", async () => {
+    mockPause.mockReturnValue()
+    mockFetch(fetchMock, concertList, true)
+    mockFetch(fetchMock, singleConcert, false)
+
+    const { user } = userRender(<App />)
+
+    await searchConcerts(user)
+    const [firstConcert] = screen.getAllByText('Play')
+    await user.click(firstConcert)
+
+    expect(screen.getByText(errorText)).toBeVisible()
+
+    await user.click(screen.getByTestId('close-error-button'))
+    expect(await screen.findByText(errorText)).not.toBeVisible()
   })
 })
